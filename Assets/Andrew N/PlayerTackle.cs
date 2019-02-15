935427c707396e8a6e1e89a468c27486167478ec
Feasting <andrewtnguyen49@gmail.com>
2019-02-14 23:46:30 -0800

    Merge from Master

---
+++ Assets/Andrew N/PlayerTackle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTackle : MonoBehaviour
+{
+    private float timeBtwTackle;
+    public float startTimeBtwAttack;
+
+    public float tackleDistance;
+    public int damage;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwTackle <= 0)
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                transform.Translate(Vector2.up * Time.deltaTime *tackleDistance);
+            }
+            timeBtwTackle = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwTackle -= Time.deltaTime;
+        }
+    }
+}
