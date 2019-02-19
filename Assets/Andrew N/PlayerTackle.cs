29e330a87edb1c0d272ad00a660a3a9326eb438f
Andrew Nguyen <andrewtnguyen49@gmail.com>
2019-02-18 16:40:35 -0800

    Dynamic Camera/ Player Tag Initial Prototypes
    The dynamic camera is currently only for 1 player and the Player Tag will need to be adjusted for the new sprite.

--- Assets/Andrew N/PlayerTackle.cs
+++ Assets/Andrew N/PlayerTackle.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class PlayerTackle : MonoBehaviour
 {
-    private float timeBtwTackle;
-    public float startTimeBtwAttack;
+    /*private Rigidbody2D rb;
+    public float tackleSpeed;
 
-    public float tackleDistance;
-    public int damage;
+    private float tackleTime;
+    public float startTackleTime;
+    private int direction; 
 
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (timeBtwTackle <= 0)
-        {
-            if (Input.GetKey(KeyCode.F))
-            {
-                transform.Translate(Vector2.up * Time.deltaTime *tackleDistance);
-            }
-            timeBtwTackle = startTimeBtwAttack;
-        }
-        else
-        {
-            timeBtwTackle -= Time.deltaTime;
-        }
+    void Start() {
+        rb = GetComponent<Rigidbody2D>;
+        tackleTime = startTackleTime;
     }
+
+    void Update() {
+        if (direction == 0) {
+            if (Input.GetKeyDown(KeyCode.D) && Input.GetKeyDown(KeyCode.Space)) {
+                direction = 1;
+            } else if (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.Space)) {
+                direction = 2;
+            }
+        } else {
+            if (tackleTime <= 2) {
+                direction = 0;
+                tackleTime = startTackleTime;
+                rb.velocity = Vector2.zero;
+            } else {
+                direction -= tackleTime.deltaTime;
+                if (direction == 1)
+                {
+                    rb.velocity = Vector2.right * tackleSpeed;
+                }
+                else if (direction == 2) {
+                    rb.velocity = Vector2.right * tackleSpeed;
+                }
+            }
+        }
+    }*/
+
 }
